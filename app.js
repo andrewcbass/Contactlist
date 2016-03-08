5e5a2e8b0987f4694199764c8153b0242c10dcd4
andrewcbass <andrew.c.bass@gmail.com>
2016-03-07 22:55:32 -0800

    delete working, edit close

--- app.js
+++ app.js
@@ -13,14 +13,60 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
+//load page
 app.get('/', function(req, res) {
   var indexPath = path.join(__dirname, 'index.html');
 
   res.sendFile(indexPath);
 })
+
+//load existing contacts into page
+app.get('/contacts', function(req, res) {
+  fs.readFile('./contacts.json', function(err, data) {
+    var contacts = JSON.parse(data);
+    console.log('contacts sent from JSON');
+    res.send(contacts);
+  });
+});
+
+//write new contact into JSON file
+app.post('/contacts', function(req, res) {
+  fs.readFile('./contacts.json', function(err, data) {
+    var contacts = JSON.parse(data);
+    contacts.push(req.body);
+
+    fs.writeFile('./contacts.json', JSON.stringify(contacts), function(err) {
+      console.log('new contact added successfuly');
+
+      res.send();
+    });
+  });
+});
+
+//edit contact, runs after saving from modal
+
+
+//delete contact from JSON file
+app.delete('/contacts/:index', function(req, res) {
+
+  fs.readFile('./contacts.json', function(err, data) {
+    var contacts = JSON.parse(data);
+    console.log('REQ.parmsr', req.params);
+    contacts.splice(req.params.index, 1);
+
+    fs.writeFile('./contacts.json', JSON.stringify(contacts), function(err) {
+      console.log('contact edited successfuly')
+
+      res.send();
+    });
+  });
+});
+
 
 var server = http.createServer(app);
 
--- public/main.js
+++ public/main.js
@@ -1,16 +1,73 @@
 'use strict';
 
+var indexEdit;
+
 angular.module('myApp', [])
   .controller('mainCtrl', function($scope, $http) {
-    $scope.conList = [];
+    $scope.getConList = function() {
+      $http({
+        method: "GET",
+        url: 'http://localhost:12345/contacts',
+      })
+      .then(function(res) {
+        console.log('RES', res);
+        $scope.conList = res.data;
+      },
+      function(err) {
+        console.log('ERR', err);
+    })
+  }
+    $scope.getConList();
 
     $scope.addContact = function () {
-      var contact = angular.copy($scope.newCon);
-      $scope.conList.push(contact);
-      $scope.newCon = {};
+      $http({
+        method: 'POST',
+        url: 'http://localhost:12345/contacts',
+        data: $scope.newCon
+      })
+      .then(function(res) {
+        //only if success, add to DOM
+        var contact = angular.copy($scope.newCon);
+        $scope.conList.push(contact);
+        $scope.newCon = {};
+      },
+      function(err) {
+        console.log('ERR', err);
+      })
+    }
+
+    $scope.editContactIndex = function (contact) {
+      indexEdit = this.$index;
+      var editedcontact = angular.copy(this.contact);
+      $scope.edit = editedcontact;
+    }
+
+    $scope.editContact = function () {
+      $http({
+        method: 'PUT',
+        url: `http://localhost:12345/${indexEdit}`,
+        data: $scope.edit
+      })
+      .then(function(res) {
+        $scope.conList[indexEdit] = $scope.edit;
+      })
     }
 
     $scope.deleteCon = function (contact) {
-      $scope.conList.splice(this.$index, 1);
+      var index = this.$index;
+
+      $http({
+        method: 'DELETE',
+        url: `http://localhost:12345/contacts/${index}`,
+      })
+      .then(function(res) {
+        //only if success, remove from DOM
+        $scope.conList.splice(index, 1);
+      },
+      function(err) {
+        console.log(err);
+      })
     }
+
+
   });
