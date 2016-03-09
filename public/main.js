56c0ce8639331e4512df07475d83b9521e657945
andrewcbass <andrew.c.bass@gmail.com>
2016-03-08 22:11:42 -0800

    refactor code and comment

--- public/main.js
+++ public/main.js
@@ -4,70 +4,59 @@ var indexEdit;
 
 angular.module('myApp', [])
   .controller('mainCtrl', function($scope, $http) {
-    $scope.getConList = function() {
-      $http({
-        method: "GET",
-        url: 'http://localhost:12345/contacts',
-      })
-      .then(function(res) {
-        console.log('RES', res);
-        $scope.conList = res.data;
-      },
-      function(err) {
-        console.log('ERR', err);
-    })
-  }
-    $scope.getConList();
 
+    //get stored contacts and load to page on visit
+    $http.get('/contacts').then(function(res){
+      console.log('RES', res);
+      $scope.conList = res.data;
+    }, function(err) {
+      console.log('ERR', err);
+    });
+
+
+    //add new contacts
     $scope.addContact = function () {
-      $http({
-        method: 'POST',
-        url: 'http://localhost:12345/contacts',
-        data: $scope.newCon
-      })
-      .then(function(res) {
-        //only if success, add to DOM
+      $http.post('/contacts', $scope.newCon)
+      .then(function(res){
+        //only if success, load to DOM
         var contact = angular.copy($scope.newCon);
         $scope.conList.push(contact);
         $scope.newCon = {};
       },
-      function(err) {
+      function(err){
         console.log('ERR', err);
       })
-    }
+    };
 
+    //grab contact idex on click of edit button
     $scope.editContactIndex = function (contact) {
       indexEdit = this.$index;
       var editedcontact = angular.copy(this.contact);
       $scope.edit = editedcontact;
     }
 
+    //edit contact info in JSON on click save edit, then in DOM
     $scope.editContact = function () {
-      $http({
-        method: 'PUT',
-        url: `http://localhost:12345/contacts/${indexEdit}`,
-        data: $scope.edit
-      })
+      $http.put(`/contacts/${indexEdit}`, $scope.edit)
       .then(function(res) {
         $scope.conList[indexEdit] = $scope.edit;
+      },
+      function(err) {
+        console.log('ERR', err);
       })
     }
 
+    //delete contact from JSON, then from DOM
     $scope.deleteCon = function (contact) {
       var index = this.$index;
 
-      $http({
-        method: 'DELETE',
-        url: `http://localhost:12345/contacts/${index}`,
-      })
+      $http.delete(`/contacts/${index}`)
       .then(function(res) {
-        //only if success, remove from DOM
         $scope.conList.splice(index, 1);
       },
       function(err) {
-        console.log(err);
+        console.log('ERR', err);
       })
     }
-
 
   });
